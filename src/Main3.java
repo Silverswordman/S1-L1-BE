8b5bcd0826d52585ad57c9ea83748a57b22829a3
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-11-27 16:56:26 +0100

    3 begin

--- src/Main3.java
+++ src/Main3.java
@@ -1,6 +1,13 @@
 public class Main3 {
 
     public static void main(String[] args) {
+        double x = 2.5;
+        double y = 3.5;
+
+        double perimeter = ((x+y)*2);
+        System.out.println("result " + perimeter);
+
+
 
     }
 }
