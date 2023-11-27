cbd94489069c6fdad32095308e6f878fabd86647
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-11-27 16:19:36 +0100

    second exercise complete

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main2 {
 
     public static void main(String[] args) {
+Scanner input =new Scanner(System.in);
+System.out.println("Type the first string");
+String firstString=input.nextLine();
+System.out.println( "First String " +  firstString);
+String secondString=input.nextLine();
+System.out.println( "Second String " +  secondString);
+String thirdString=input.nextLine();
+System.out.println( "Third String " +  thirdString);
 
+String firstPhrase = " " + firstString + " " + secondString+ " " +  thirdString;
+System.out.println ( "Ordered strings" +  firstPhrase);
+
+String secondPhrase = " "+thirdString + " " + firstString+ " " +  secondString;
+System.out.println ( "Unordered strings" + secondPhrase);
+ input.close();
     }
 }
